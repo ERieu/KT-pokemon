abb9939d9bcb28901b8ad797c5e85f98342bf4a9
ERieu <evan.rieu@gmail.com>
2024-04-05 11:59:11 +0200

    first commi

---
+++ src/pokemonFeu.kt
@@ -0,0 +1,17 @@
+class pokemonFeu {
+    var nom = ""
+    var hp = 0
+    var atk = 0
+
+    fun isDead(): Boolean {
+        var statut = false
+
+        if (this.hp == 0) {
+            statut = true
+        }
+
+        return statut
+    }
+
+    fun attaquer() {}
+}
---
+++ src/pokemonPlante.kt
@@ -0,0 +1,15 @@
+class pokemonPlante {
+    var nom = ""
+    var hp = 0
+    var atk = 0
+
+    fun isDead(): Boolean {
+        var statut = false
+
+        if (this.hp == 0) {
+            statut = true
+        }
+
+        return statut
+    }
+}
